e1c181a50da1d9ff19fc8c665fd1ed82cac8a4e3
Simon Harrington <simon.a.harrington@gmail.com>
2026-07-19 15:04:08 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+git clone https://github.com/yourusername/dotfiles.git ~/dotfiles
+mkdir -p ~/.vim
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/autoload ~/.vim/autoload
+ln -s ~/dotfiles/vim/colors ~/.vim/colors
+ln -s ~/dotfiles/vim/doc ~/.vim/doc
+echo "dotfiles copied successfully"
